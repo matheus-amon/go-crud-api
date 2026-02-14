044886394393812dc5c4188cd981b1b187fba79e
Matheus Amon <matheus.amon@singlesoftware.com>
2026-02-14 19:27:07 -0300

    add get users controller and other layes

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-crud-api/controller"
+	"go-crud-api/db"
+	"go-crud-api/repository"
+	"go-crud-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	UserRepository := repository.NewUserRepository(dbConnection)
+	UserUseCase := usecase.NewUserUseCase(UserRepository)
+	UserController := controller.NewUserController(UserUseCase)
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("users/", UserController.GetUsers)
 
 	server.Run(":8000")
 
